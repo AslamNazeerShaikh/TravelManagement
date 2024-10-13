771afb440ded4075f1ba3d85ad1638345aa496c2
Aslam Shaikh <aslam.nazeer.shaikh@outlook.in>
2024-10-14 00:31:07 +0530

    Added Some Simple Model Validations

--- src/TravelManagement.Domain/Entities/Tour.cs
+++ src/TravelManagement.Domain/Entities/Tour.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,26 @@ namespace TravelManagement.Domain.Entities
 {
     public class Tour
     {
+        [Key]
         public int Id { get; set; }
-        public string? Name { get; set; } = string.Empty;
-        public string? FromLocation { get; set; } = string.Empty;
-        public string? ToLocation { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Tour Name is required.")]
+        [StringLength(100, ErrorMessage = "Name should be a maximum of 100 characters.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "From Location is required.")]
+        public string FromLocation { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "To Location is required.")]
+        public string ToLocation { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Start Date is required.")]
         public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "End Date is required.")]
         public DateTime EndDate { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive value.")]
         public decimal Price { get; set; }
     }
 }
