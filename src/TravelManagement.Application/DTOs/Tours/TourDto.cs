958a0e20fce09cc79eb4311549bf67051aeed932
Aslam Shaikh <aslam.nazeer.shaikh@outlook.in>
2024-10-18 01:23:56 +0530

    Added Docs

---
+++ src/TravelManagement.Application/DTOs/Tours/TourDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelManagement.Application.DTOs.Tours
+{
+    public record TourDto
+    {
+        Id                
+        Name
+        FromLocation
+        ToLocation 
+        StartDate
+        EndDate
+        Price 
+        CreatedBy 
+        CreatedDate 
+        UpdatedBy 
+        UpdatedDate 
+}
+}
